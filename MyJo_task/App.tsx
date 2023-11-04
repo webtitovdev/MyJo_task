337004531fabbdc0d163fc2cc23e25f650d50aae
Dev Coder <devcoder@progforce.com>
2023-11-04 12:23:05 +0300

    [CREATE] Create react native project for MyJo

---
+++ MyJo_task/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
